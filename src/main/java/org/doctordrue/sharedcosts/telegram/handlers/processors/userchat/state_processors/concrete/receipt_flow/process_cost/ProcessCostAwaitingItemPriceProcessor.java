b0e6a5004af0a71d0e70842a2e233fdfb3dec59e
Andrey Barantsev <andrey.barantsev@ukg.com>
2022-07-17 22:14:39 +0300

    refactoring of single state update processors

--- src/main/java/org/doctordrue/sharedcosts/telegram/handlers/processors/userchat/state_processors/concrete/receipt_flow/process_cost/ProcessCostAwaitingItemPriceProcessor.java
+++ src/main/java/org/doctordrue/sharedcosts/telegram/handlers/processors/userchat/state_processors/concrete/receipt_flow/process_cost/ProcessCostAwaitingItemPriceProcessor.java
@@ -1,20 +1,30 @@
 package org.doctordrue.sharedcosts.telegram.handlers.processors.userchat.state_processors.concrete.receipt_flow.process_cost;
 
+import org.doctordrue.sharedcosts.business.services.processing.ParticipationProcessingService;
+import org.doctordrue.sharedcosts.data.entities.Participation;
 import org.doctordrue.sharedcosts.exceptions.BaseException;
+import org.doctordrue.sharedcosts.exceptions.parse.MoneyFormatException;
 import org.doctordrue.sharedcosts.telegram.data.entities.UserChatSession;
 import org.doctordrue.sharedcosts.telegram.handlers.processors.userchat.state_processors.base.BaseSingleStateUserChatProcessor;
 import org.doctordrue.sharedcosts.telegram.session.userchat.UserChatSessionWorker;
 import org.doctordrue.sharedcosts.telegram.session.userchat.UserChatState;
+import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.Message;
 
 /**
  * @author Andrey_Barantsev
  * 6/24/2022
  **/
+@Component
 public class ProcessCostAwaitingItemPriceProcessor extends BaseSingleStateUserChatProcessor {
 
-   public ProcessCostAwaitingItemPriceProcessor(UserChatSessionWorker sessionWorker) {
+   private static final String AMOUNT_REGEX = "^\\d+(\\.\\d{1,2})?$";
+
+   private final ParticipationProcessingService participationProcessingService;
+
+   public ProcessCostAwaitingItemPriceProcessor(UserChatSessionWorker sessionWorker, ParticipationProcessingService participationProcessingService) {
       super(sessionWorker, UserChatState.PROCESS_COST_SELECTING_ACTION);
+      this.participationProcessingService = participationProcessingService;
    }
 
    @Override
@@ -24,6 +34,17 @@ public class ProcessCostAwaitingItemPriceProcessor extends BaseSingleStateUserCh
 
    @Override
    protected void verifyMessage(Message message, UserChatSession session) throws BaseException {
+      String text = message.getText();
+      if (text.matches(AMOUNT_REGEX)) {
+         Double amount = Double.parseDouble(text);
 
+         this.participationProcessingService.processNew(new Participation()
+                 .setCost(session.getSelectedCost())
+                 .setAmount(amount)
+                 .setName(session.getTempParticipationName()), true);
+         this.updateSession(session, s -> s.setTempParticipationName(null));
+      } else {
+         throw new MoneyFormatException();
+      }
    }
 }
--- src/main/java/org/doctordrue/sharedcosts/telegram/handlers/processors/userchat/state_processors/concrete/receipt_flow/process_cost/ProcessCostSelectingCurrencyProcessor.java
+++ src/main/java/org/doctordrue/sharedcosts/telegram/handlers/processors/userchat/state_processors/concrete/receipt_flow/process_cost/ProcessCostSelectingCurrencyProcessor.java
@@ -1,6 +1,10 @@
 package org.doctordrue.sharedcosts.telegram.handlers.processors.userchat.state_processors.concrete.receipt_flow.process_cost;
 
+import java.util.Optional;
+
+import org.doctordrue.sharedcosts.data.entities.Currency;
 import org.doctordrue.sharedcosts.exceptions.BaseException;
+import org.doctordrue.sharedcosts.exceptions.currency.CurrencyNotFoundException;
 import org.doctordrue.sharedcosts.telegram.data.entities.UserChatSession;
 import org.doctordrue.sharedcosts.telegram.handlers.processors.userchat.state_processors.base.BaseSingleStateUserChatProcessor;
 import org.doctordrue.sharedcosts.telegram.session.userchat.UserChatSessionWorker;
@@ -21,11 +25,18 @@ public class ProcessCostSelectingCurrencyProcessor extends BaseSingleStateUserCh
 
    @Override
    protected String onSuccessMessage(UserChatSession session) {
-      return null;
+      return String.format("Валюта чека: %s", session.getCurrency().getFullName());
    }
 
    @Override
    protected void verifyMessage(Message message, UserChatSession session) throws BaseException {
-
+      String text = message.getText();
+      Optional<Currency> maybeCurrency = session.getAvailableCurrencies().stream().filter(c -> c.getShortName().equals(text)).findFirst();
+      if (maybeCurrency.isPresent()) {
+         // can create new cost
+         this.updateSession(session, s -> s.setCurrency(maybeCurrency.get()));
+      } else {
+         throw new CurrencyNotFoundException(text);
+      }
    }
 }
